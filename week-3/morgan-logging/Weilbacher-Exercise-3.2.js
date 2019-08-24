eaccc0b4beeded941e29c2ac5cf81896f2c9c8c4
Andrew Philip Weilbacher <andrew.weilbacher@gmail.com>
2019-08-24 13:09:01 -0400

    exercise 3.2

---
+++ week-3/morgan-logging/Weilbacher-Exercise-3.2.js
@@ -0,0 +1,26 @@
+var express = require("express");
+
+var http = require("http");
+
+var path = require("path");
+
+var logger = require("morgan");
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views")); // Tell Express the views are in the 'views' directory
+
+app.set("view engine","ejs"); // Tell Express to use the EJS view engine
+
+app.use(logger("short"));
+
+app.get("/", function(request, response) {
+    response.render("index", {
+        message: "This is your Morgan Logger Example from Andrew Weilbacher!"
+    });
+});
+
+http.createServer(app).listen(3000, function(){
+    console.log("Application started on port 3000.");
+});
+
