c27c3d0291030e2d35c052e795c171180368448f
Andrew Philip Weilbacher <andrew.weilbacher@gmail.com>
2019-09-23 20:36:03 -0400

    week-7

--- EMS/models/employee.js
+++ EMS/models/employee.js
@@ -1,8 +1,8 @@
 /*
 ============================================
 ; Title:  employee.js
-; Author: Griselda Balmaceda
-; Date:   07 April 2019
+; Author: Andrew Weilbacher
+; Date:   23 September 2019
 ; Description: File for the Employee database model
 ;===========================================
 */
